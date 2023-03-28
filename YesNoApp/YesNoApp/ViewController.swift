5ff559cfe3a2f3047c144121ee85bfb49139b004
Daniil Pavlenko <danyy.pavl@gmail.com>
2023-03-28 11:11:14 +0300

    init project

---
+++ YesNoApp/YesNoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YesNoApp
+//
+//  Created by Даниил Павленко on 28.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
